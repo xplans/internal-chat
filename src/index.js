63c4f24ff463e82c5bc36ea9974200d9e67afdc0
xplans <ahuimsn@gmail.com>
2025-01-13 16:54:22 +0800

    增加客户端页面

--- src/index.js
+++ src/index.js
@@ -8,8 +8,59 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import index from './www/index.html';
+import indexjs from './www/index.js.txt';
+import stylecss from './www/style.css.txt';
+import xchatuserjs from './www/xchatuser.js.txt';
+
+
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+    // 请求方法
+    const method = request.method;
+    // 请求路径
+    const url = new URL(request.url);
+    console.log('==> url: ', url);
+    console.log('==> method: ', method);
+
+    if (method === 'GET') {
+      // Get请求参数
+      const getParams = url.searchParams;
+      console.log('==> params: ', '' + getParams);
+      switch(url.pathname) {
+        case '/':
+        {
+          return new Response(index, {
+            headers: { 'Content-Type': 'text/html' }
+          });
+        }
+        case '/index.js':
+        {
+          return new Response(indexjs, {
+            headers: { 'Content-Type': 'application/x-javascript' }
+          });
+        }
+        case '/style.css':
+        {
+          return new Response(stylecss, {
+            headers: { 'Content-Type': 'text/css' }
+          });
+        }
+        case '/xchatuser.js':
+        {
+          return new Response(xchatuserjs, {
+            headers: { 'Content-Type': 'application/x-javascript' }
+          });
+        }
+      }
+
+    } else if (method === 'POST') {
+      // Post请求参数
+      const postParams = await request.json();
+      return new Response(JSON.stringify(postParams));
+    }
+    return new Response('Hello World!');
+  },
+
 };
